5b00d774697ee196e54e580859a2133b618d9d39
Mikail <mikailsyed@yahoo.com>
2025-01-02 13:09:12 -0500

    Var calc

--- leastsquare.py
+++ leastsquare.py
@@ -6,10 +6,16 @@ def leastSquare(n: int, x: list, y: list):
     # Through partial derivatives, we can assume a = Cov(x,y) / Var(x)
 
     x_bar = sum(x) / len(x)
+    y_bar = sum(y) / len(y)
 
     cov = 0
     var = 0
     
     # Cov calculation
+    for i in range(n):
+        eq = (x[i] - x_bar) * (y[i] - y_bar)
+        cov += eq
+    
+    # Var calculation
     for i in range(n):
         
