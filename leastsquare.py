3c6d4c659b8636446fe6a54ee0873c81a125e45e
Mikail <mikailsyed@yahoo.com>
2025-01-02 13:06:41 -0500

    Cov calc

--- leastsquare.py
+++ leastsquare.py
@@ -1,2 +1,15 @@
 # The purpose of this program is to demonstrate the Principle of Least Squares Algorithm. Run via 'python3 leastsquare.py'
 
+def leastSquare(n: int, x: list, y: list):
+    # Sum of squared errors (SSE): (y[i] - (a * x[i] + b))^2 for i in range(n)
+
+    # Through partial derivatives, we can assume a = Cov(x,y) / Var(x)
+
+    x_bar = sum(x) / len(x)
+
+    cov = 0
+    var = 0
+    
+    # Cov calculation
+    for i in range(n):
+        
